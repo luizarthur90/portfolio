867bdf19fbb06dfe05b2150b58b166d0734e9c84
HOSTSERVERDOBRASIL <contato@hostserver.com.br>
2025-03-26 19:56:02 -0300

    Corrigindo os detalhes

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -1,5 +1,10 @@
 import CaseStudy from '../CaseStudy/CaseStudy';
 import './Section2.css';
+import CoverSK from "/public/Cover_SK.jpg"
+import CoverRostering from '/public/Cover_Rostering.jpg'
+import CoverClic from '/public/Cover_Clic.png'
+import CoverVittude from '/public/Cover_Vittude.svg'
+
 
 function Section2() {
 
@@ -24,28 +29,28 @@ function Section2() {
                 <CaseStudy
                     Name={'The protagonist skateboarder and the most engaged teacher'}
                     Description={'Gamified training and monitoring platform for skateboarders and teachers'}
-                    ImgSource={'src/assets/Cover_SK.jpg'}
+                    ImgSource={CoverSK}
                     label={'Edtech'}
                     Link={"https://luizarthur.notion.site/Gamified-training-and-monitoring-platform-for-skateboarders-and-teachers-1bc78a6c271e8081bf7dc51c8a837674"}
                     ImgLink={"https://luizarthur.notion.site/Gamified-training-and-monitoring-platform-for-skateboarders-and-teachers-1bc78a6c271e8081bf7dc51c8a837674"} />
                 <CaseStudy
                     Name={'Optimized Work Schedule Management'}
                     Description={'Restructuring of App Rostering flow and screens'}
-                    ImgSource={'src/assets/Cover_Rostering.jpg'}
+                    ImgSource={CoverRostering}
                     label={'HRtech'}
                     Link={'https://luizarthur.notion.site/Como-melhoramos-a-jornada-do-gestor-de-times-e-nosso-processo-de-recrutamento-6bc6058354944959a191c1567133c13c'}
                     ImgLink={'https://luizarthur.notion.site/Como-melhoramos-a-jornada-do-gestor-de-times-e-nosso-processo-de-recrutamento-6bc6058354944959a191c1567133c13c'} />
                 <CaseStudy
                     Name={'A new Telemedicine experience'}
                     Description={'Rethinking patient flow in a telemedicine app'}
-                    ImgSource={'src/assets/Cover_Clic.png'}
+                    ImgSource={CoverClic}
                     label={'Healthtech'}
                     Link={'https://luizarthur.notion.site/New-Experience-of-a-Telemedicine-Service-1dc11eb5594e4a70aa5041b2d2b2fd2a'}
                     ImgLink={'https://luizarthur.notion.site/New-Experience-of-a-Telemedicine-Service-1dc11eb5594e4a70aa5041b2d2b2fd2a'} />
                 <CaseStudy
                     Name={'Key value deliveries of health apps'}
                     Description={'An UX Research of a Pyscotherapy App'}
-                    ImgSource={'src/assets/Cover_Vittude.svg'}
+                    ImgSource={CoverVittude}
                     label={'Healthtech'}
                     Link={'https://drive.google.com/file/d/1ZgZbz9ZhbNz12XeqOtv5jwl-X-Pm_urc/view?usp=drive_link'}
                     ImgLink={'https://drive.google.com/file/d/1ZgZbz9ZhbNz12XeqOtv5jwl-X-Pm_urc/view?usp=drive_link'} />
